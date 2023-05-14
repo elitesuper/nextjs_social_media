6ba253b9ec33c624bc5e9c7def5e4a117e0a498a
elitesuper <tonylobbin7712@gmail.com>
2023-05-14 15:20:04 -0700

    update head and foot

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -332,15 +332,12 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="w-full">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl text-center">
-            We invest in the world’s potential
+            This is AI Caption Generator
           </h1>
           <div className='w-full mt-6'>
             <div className='max-w-2xl mx-auto'>
               <p className="text-lg text-zinc-600 dark:text-zinc-400 text-center">
-                I’m Spencer, a software designer and entrepreneur based in New York
-                City. I’m the founder and CEO of Planetaria, where we develop
-                technologies that empower regular people to explore space on their
-                own terms.
+                AI Caption Generator is a tool that uses artificial intelligence to automatically generate captions or descriptions for images. 
               </p>
             </div>
           </div>
